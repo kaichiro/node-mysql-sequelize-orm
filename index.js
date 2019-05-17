18a822ce4824f426cbd5958d161e7e06de646761
kaichiro <kcfg23@gmail.com>
2019-05-17 12:23:12 -0300

    Adicionando arquivos principais do projeto

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const bodyParser = require('body-parser')
+
+require('dotenv/config')
+
+const model = require('./models/index')
+const pessoas = require('./routes/pessoas')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index'))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/pessoas', pessoas)
+
+model.sequelize.sync({ force: false }).then(() => {
+    const port = process.env.PORT || 3000
+    const webLink = 'http://localhost'
+    app.listen(port, () => console.log(`\nApp is run in ${webLink}:${port}\n`))
+})
